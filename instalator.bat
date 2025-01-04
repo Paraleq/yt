3e923eb56d6ed212cb41e767c4120bfb2044223f
Paraleq <101002749+Paraleq@users.noreply.github.com>
2025-01-04 22:31:52 +0100

    Update and rename instalator.bash to instalator.bat

---
+++ instalator.bat
@@ -0,0 +1,5 @@
+@echo off
+echo Instalowanie wymaganych bibliotek Python...
+pip install google-auth google-auth-oauthlib google-auth-httplib2 google-api-python-client requests
+echo Instalacja zako\u0144czona. Naci\u015bnij dowolny klawisz, aby zamkn\u0105\u0107.
+pause
